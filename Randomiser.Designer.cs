1529e061abf4978e14f1d7317acb217c3a302c93
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2021-10-20 19:47:18 +0100

    Done for today

--- Randomiser.Designer.cs
+++ Randomiser.Designer.cs
@@ -31,10 +31,17 @@ namespace BlueFireRando
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Randomiser));
             this.Randomise = new System.Windows.Forms.Button();
-            this.RandoOptions = new System.Windows.Forms.CheckedListBox();
-            this.Disclaimer = new System.Windows.Forms.TextBox();
             this.UmbyPet = new System.Windows.Forms.PictureBox();
+            this.Spirits = new System.Windows.Forms.CheckBox();
+            this.Mix = new System.Windows.Forms.CheckBox();
+            this.Weapons = new System.Windows.Forms.CheckBox();
+            this.KeyItems = new System.Windows.Forms.CheckBox();
+            this.Keys = new System.Windows.Forms.CheckBox();
+            this.Items = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Disclaimer = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.UmbyPet)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Randomise
@@ -46,7 +53,7 @@ namespace BlueFireRando
             this.Randomise.FlatAppearance.BorderSize = 0;
             this.Randomise.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Randomise.Font = new System.Drawing.Font("Cinzel", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Randomise.Location = new System.Drawing.Point(1524, 840);
+            this.Randomise.Location = new System.Drawing.Point(1007, 568);
             this.Randomise.Margin = new System.Windows.Forms.Padding(4);
             this.Randomise.Name = "Randomise";
             this.Randomise.Size = new System.Drawing.Size(343, 167);
@@ -55,26 +62,119 @@ namespace BlueFireRando
             this.Randomise.UseVisualStyleBackColor = false;
             this.Randomise.Click += new System.EventHandler(this.Randomise_Click);
             // 
-            // RandoOptions
+            // UmbyPet
             // 
-            this.RandoOptions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.RandoOptions.BackColor = System.Drawing.SystemColors.WindowText;
-            this.RandoOptions.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.RandoOptions.Font = new System.Drawing.Font("Cinzel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RandoOptions.ForeColor = System.Drawing.Color.LightGreen;
-            this.RandoOptions.FormattingEnabled = true;
-            this.RandoOptions.Items.AddRange(new object[] {
-            "Mix Randomisation?*",
-            "Randomise Spirits?",
-            "Randomise Weapons?*",
-            "Randomise Key items?*",
-            "Randomise Keys?*",
-            "Randomise Items?*"});
-            this.RandoOptions.Location = new System.Drawing.Point(1379, 283);
-            this.RandoOptions.Name = "RandoOptions";
-            this.RandoOptions.Size = new System.Drawing.Size(488, 282);
-            this.RandoOptions.TabIndex = 2;
-            this.RandoOptions.SelectedIndexChanged += new System.EventHandler(this.RandoOptions_SelectedIndexChanged);
+            this.UmbyPet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UmbyPet.BackColor = System.Drawing.Color.Transparent;
+            this.UmbyPet.Image = ((System.Drawing.Image)(resources.GetObject("UmbyPet.Image")));
+            this.UmbyPet.InitialImage = ((System.Drawing.Image)(resources.GetObject("UmbyPet.InitialImage")));
+            this.UmbyPet.Location = new System.Drawing.Point(0, 492);
+            this.UmbyPet.Name = "UmbyPet";
+            this.UmbyPet.Size = new System.Drawing.Size(315, 294);
+            this.UmbyPet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.UmbyPet.TabIndex = 4;
+            this.UmbyPet.TabStop = false;
+            // 
+            // Spirits
+            // 
+            this.Spirits.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Spirits.AutoSize = true;
+            this.Spirits.BackColor = System.Drawing.Color.Transparent;
+            this.Spirits.Font = new System.Drawing.Font("Cinzel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Spirits.ForeColor = System.Drawing.SystemColors.Control;
+            this.Spirits.Location = new System.Drawing.Point(3, 430);
+            this.Spirits.Name = "Spirits";
+            this.Spirits.Size = new System.Drawing.Size(368, 48);
+            this.Spirits.TabIndex = 5;
+            this.Spirits.Text = "Randomise Spirits?";
+            this.Spirits.UseVisualStyleBackColor = false;
+            this.Spirits.CheckedChanged += new System.EventHandler(this.Spirits_CheckedChanged);
+            // 
+            // Mix
+            // 
+            this.Mix.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Mix.AutoSize = true;
+            this.Mix.BackColor = System.Drawing.Color.Transparent;
+            this.Mix.Font = new System.Drawing.Font("Cinzel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mix.ForeColor = System.Drawing.SystemColors.Control;
+            this.Mix.Location = new System.Drawing.Point(3, 160);
+            this.Mix.Name = "Mix";
+            this.Mix.Size = new System.Drawing.Size(417, 48);
+            this.Mix.TabIndex = 6;
+            this.Mix.Text = "Mix Randomisation?*";
+            this.Mix.UseVisualStyleBackColor = false;
+            // 
+            // Weapons
+            // 
+            this.Weapons.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Weapons.AutoSize = true;
+            this.Weapons.BackColor = System.Drawing.Color.Transparent;
+            this.Weapons.Font = new System.Drawing.Font("Cinzel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Weapons.ForeColor = System.Drawing.SystemColors.Control;
+            this.Weapons.Location = new System.Drawing.Point(3, 214);
+            this.Weapons.Name = "Weapons";
+            this.Weapons.Size = new System.Drawing.Size(426, 48);
+            this.Weapons.TabIndex = 7;
+            this.Weapons.Text = "Randomise Weapons?*";
+            this.Weapons.UseVisualStyleBackColor = false;
+            // 
+            // KeyItems
+            // 
+            this.KeyItems.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.KeyItems.AutoSize = true;
+            this.KeyItems.BackColor = System.Drawing.Color.Transparent;
+            this.KeyItems.Font = new System.Drawing.Font("Cinzel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KeyItems.ForeColor = System.Drawing.SystemColors.Control;
+            this.KeyItems.Location = new System.Drawing.Point(3, 268);
+            this.KeyItems.Name = "KeyItems";
+            this.KeyItems.Size = new System.Drawing.Size(431, 48);
+            this.KeyItems.TabIndex = 8;
+            this.KeyItems.Text = "Randomise Key Items?*";
+            this.KeyItems.UseVisualStyleBackColor = false;
+            // 
+            // Keys
+            // 
+            this.Keys.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Keys.AutoSize = true;
+            this.Keys.BackColor = System.Drawing.Color.Transparent;
+            this.Keys.Font = new System.Drawing.Font("Cinzel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Keys.ForeColor = System.Drawing.SystemColors.Control;
+            this.Keys.Location = new System.Drawing.Point(3, 322);
+            this.Keys.Name = "Keys";
+            this.Keys.Size = new System.Drawing.Size(346, 48);
+            this.Keys.TabIndex = 9;
+            this.Keys.Text = "Randomise Keys?*";
+            this.Keys.UseVisualStyleBackColor = false;
+            // 
+            // Items
+            // 
+            this.Items.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Items.AutoSize = true;
+            this.Items.BackColor = System.Drawing.Color.Transparent;
+            this.Items.Font = new System.Drawing.Font("Cinzel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Items.ForeColor = System.Drawing.SystemColors.Control;
+            this.Items.Location = new System.Drawing.Point(3, 376);
+            this.Items.Name = "Items";
+            this.Items.Size = new System.Drawing.Size(362, 48);
+            this.Items.TabIndex = 10;
+            this.Items.Text = "Randomise Items?*";
+            this.Items.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Controls.Add(this.Disclaimer);
+            this.flowLayoutPanel1.Controls.Add(this.Mix);
+            this.flowLayoutPanel1.Controls.Add(this.Weapons);
+            this.flowLayoutPanel1.Controls.Add(this.KeyItems);
+            this.flowLayoutPanel1.Controls.Add(this.Keys);
+            this.flowLayoutPanel1.Controls.Add(this.Items);
+            this.flowLayoutPanel1.Controls.Add(this.Spirits);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(949, 43);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(446, 514);
+            this.flowLayoutPanel1.TabIndex = 11;
             // 
             // Disclaimer
             // 
@@ -83,27 +183,14 @@ namespace BlueFireRando
             this.Disclaimer.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Disclaimer.Font = new System.Drawing.Font("Cinzel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Disclaimer.ForeColor = System.Drawing.Color.LightGreen;
-            this.Disclaimer.Location = new System.Drawing.Point(1379, 118);
+            this.Disclaimer.Location = new System.Drawing.Point(3, 3);
             this.Disclaimer.Multiline = true;
             this.Disclaimer.Name = "Disclaimer";
-            this.Disclaimer.Size = new System.Drawing.Size(488, 172);
+            this.Disclaimer.Size = new System.Drawing.Size(437, 151);
             this.Disclaimer.TabIndex = 3;
             this.Disclaimer.Text = "Items marked with a * are features yet to be implemented and if you do tick them " +
     "it will have no effect.\r\nMix randomisation mean key items can be in weapon locat" +
     "ions etc.";
-            // 
-            // UmbyPet
-            // 
-            this.UmbyPet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.UmbyPet.BackColor = System.Drawing.Color.Transparent;
-            this.UmbyPet.Image = ((System.Drawing.Image)(resources.GetObject("UmbyPet.Image")));
-            this.UmbyPet.InitialImage = ((System.Drawing.Image)(resources.GetObject("UmbyPet.InitialImage")));
-            this.UmbyPet.Location = new System.Drawing.Point(0, 764);
-            this.UmbyPet.Name = "UmbyPet";
-            this.UmbyPet.Size = new System.Drawing.Size(315, 294);
-            this.UmbyPet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.UmbyPet.TabIndex = 4;
-            this.UmbyPet.TabStop = false;
             // 
             // Randomiser
             // 
@@ -112,10 +199,9 @@ namespace BlueFireRando
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.ClientSize = new System.Drawing.Size(1407, 783);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.UmbyPet);
-            this.Controls.Add(this.Disclaimer);
-            this.Controls.Add(this.RandoOptions);
             this.Controls.Add(this.Randomise);
             this.Font = new System.Drawing.Font("Cinzel", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -126,17 +212,24 @@ namespace BlueFireRando
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.BFRandoGUI_Load);
             ((System.ComponentModel.ISupportInitialize)(this.UmbyPet)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button Randomise;
-        private System.Windows.Forms.CheckedListBox RandoOptions;
-        private System.Windows.Forms.TextBox Disclaimer;
         private System.Windows.Forms.PictureBox UmbyPet;
+        private System.Windows.Forms.CheckBox Spirits;
+        private System.Windows.Forms.CheckBox Mix;
+        private System.Windows.Forms.CheckBox Weapons;
+        private System.Windows.Forms.CheckBox KeyItems;
+        private System.Windows.Forms.CheckBox Keys;
+        private System.Windows.Forms.CheckBox Items;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox Disclaimer;
     }
 }
 
--- Randomiser.cs
+++ Randomiser.cs
@@ -22,51 +22,16 @@ namespace BlueFireRando
         void RandomiseSpirits()
         {
             //Load umap/enum
-            AssetWriter y = new AssetWriter(@".\Baseassets\Blue Fire\Content\Enums\Spirits.uasset", null, null);
+            UAsset y = new UAsset(@".\Baseassets\Blue Fire\Content\Enums\Spirits.uasset", UE4Version.VER_UE4_25);
             //MessageBox.Show($"Data preserved:{(y.VerifyParsing() ? "yes" : "no")}");
-            //Go through all data blocks
-            for (int i = 0; i < y.data.categories.Count; i++)
+            //Only one export so for loop isn't needed
+            Export baseUs = y.Exports[0];
+            if (baseUs is NormalExport us)
             {
-                Category category = y.data.categories[i];
-                if (category is NormalCategory cat)
-                {
-                    //Loop through subcategories
-                    for (int j = 0; j < cat.Data.Count; j++)
-                    {
-                        //is an enum key?
-                        if (cat.Data[j].Name == "Key(0)" && cat.Data[j] is StructPropertyData bob)
-                        {
-                            //get the name
-                            int temp = 0;
-                            bool valid = false;
-                            string[] PossibleIndexes = new string[30];
-                            string[] UsedIndexes = new string[30];
-                            var index = bob.Value as NamePropertyData;
-                            for (int k = 0; k < PossibleIndexes.Length; k++)
-                            {
-                                PossibleIndexes[k] = Convert.ToString(k);
-                            }
-                            while (valid == false)
-                            {
-                                valid = false;
-                                temp = rndm.Next(0, 8);
-                                if (UsedIndexes.Contains(PossibleIndexes[temp]))
-                                {
-                                    continue;
-                                }
-                                else
-                                {
-                                    valid = true;
-                                }
-                            }
-                            index.Value = $"NewEnumerator{PossibleIndexes[temp]}(0)";
-                            UsedIndexes[j] = PossibleIndexes[temp];
-                        }
-                    }
-                }
+                var newValues = new NamePropertyData(FName.FromString("Key"));
             }
-            y.Write(@".\Randomiser_P\Blue Fire\Content\Enums\Spirits.uasset");
-            MessageBox.Show("Spirits Randomised");
+            //y.Write(@".\Randomiser_P\Blue Fire\Content\Enums\Spirits.uasset");
+            //MessageBox.Show("Spirits Randomised");
         }
 
         public Randomiser()
@@ -91,15 +56,19 @@ namespace BlueFireRando
             }
             else
             {
-                MessageBox.Show("Randomisation complete! You'll find the pak file in the root directory");
+                //System.Diagnostics.Process.Start(@".\Packing.bat");
+                //MessageBox.Show("Randomisation complete! You'll find the pak file next to the application");
             }
 
         }
 
-        private void RandoOptions_SelectedIndexChanged(object sender, EventArgs e)
+        private void Spirits_CheckedChanged(object sender, EventArgs e)
         {
-            randomiseSpirits = true;
-            nothingSelected = false;
+            if (Spirits.Checked)
+            {
+                randomiseSpirits = true;
+                nothingSelected = false;
+            }
         }
     }
 }
