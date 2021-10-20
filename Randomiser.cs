8665a0eb37459c4946c28c9c81ab389dc1d09d02
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2021-10-20 11:40:14 +0100

    Added some checkpoint logic which I realised isn't going to work

--- Randomiser.cs
+++ Randomiser.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using UAssetAPI;
+using UAssetAPI.PropertyTypes;
+using UAssetAPI.StructTypes;
 
 namespace BlueFireRando
 {
@@ -14,24 +16,70 @@ namespace BlueFireRando
     public partial class Randomiser : Form
     {
         Random rndm = new Random();
-        int[] AllIndexes;
-        int[] UsedIndexes;
+        bool randomiseCheckpoints = false;
         //TODO write out logic for these
-        void RandomiseItems(string filepath)
+        void RandomiseCheckpoints()
         {
-            //Load umap
+            //Load umap/enum
+            AssetWriter y = new AssetWriter(@".\Base\Blue Fire\Content\Enums\CheckPoints.uasset", null, null);
+            MessageBox.Show($"Data preserved:{(y.VerifyParsing() ? "yes" : "no")}");
+            //Go through all data blocks
+            for (int i = 0; i < y.data.categories.Count; i++)
+            {
+                Category category = y.data.categories[i];
+                if (category is NormalCategory cat)
+                {
+                    //Loop through subcategories
+                    for (int j = 0; j < cat.Data.Count; j++)
+                    {
+                        //is an enum key?
+                        if (cat.Data[j].Name == "Key(0)" && cat.Data[j] is StructPropertyData bob)
+                        {
+                            //get the name
+                            int temp=0;
+                            bool valid=false;
+                            string[] PossibleIndexes = {"0","1","2","3","5","6","17","18","19"};
+                            string[] UsedIndexes=new string[9];
+                            var index = bob.Value as NamePropertyData;
+                            while (valid == false)
+                            {
+                                temp = rndm.Next(0, 8);
+                                if (UsedIndexes.Contains(PossibleIndexes[temp]))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    valid = true;
+                                }
+                            }
+                            index.Value = $"NewEnumerator{PossibleIndexes[temp]}(0)";
+                            valid = false;
+                        }
+                    }
+                }
+            }
+            y.Write(@".\Blue Fire\Content\Enums\CheckPoints.uasset");
+        }
+        void RandomiseEnums(string filepath)
+        {
+            //Load umap/enum
             AssetWriter y = new AssetWriter(filepath, null, null);
+            MessageBox.Show($"Data preserved:{(y.VerifyParsing() ? "yes" : "no")}");
             //Go through all data blocks
             for(int i = 0; i < y.data.categories.Count; i++)
             {
                 Category category = y.data.categories[i];
                 if(category is NormalCategory cat)
                 {
-
                     //Loop through subcategories
+                    for(int j = 0; j < cat.Data.Count; j++)
+                    {
+
+                    }
                 }
             }
-            int temp=rndm.Next(0, AllIndexes.Length - 1);
+            //int temp=rndm.Next(0, AllIndexes.Length - 1);
             
         }
 
@@ -48,11 +96,15 @@ namespace BlueFireRando
         private void Randomise_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Randomising requested content...");
+            if (randomiseCheckpoints==true)
+            {
+                RandomiseCheckpoints();
+            }
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void RandoOptions_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            randomiseCheckpoints = true;
         }
     }
 }
