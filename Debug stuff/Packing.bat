79dabaa83d2c74f43b2f572c92715610601ce358
bananaturtlesandwich <afcarreyette@gmail.com>
2021-11-04 21:10:05 +0000

    Organised a bunch of stuff and started experimenting with replacing exports

---
+++ Debug stuff/Packing.bat
@@ -0,0 +1,12 @@
+@echo off
+@if "%~dp0\Randomiser_P"=="" goto skip
+
+@setlocal enableextensions
+@pushd %~dp0
+@echo "%~dp0\Randomiser_P\*.*" "..\..\..\*.*" >filelist.txt
+.\UnrealPak.exe "Randomiser_P.pak" -create=filelist.txt -compress
+del filelist.txt
+@popd
+Rem @pause
+
+:skip
--- Umaps.cs
+++ Umaps.cs
@@ -14,9 +14,10 @@ namespace BlueFireRando
         {
             //Load umap
             UAsset y = new UAsset(filepath, UE4Version.VER_UE4_25);
+            UAsset z = new UAsset(@".\Baseassets\World\A02_ArcaneTunnels\A02_GameIntro_FirstVoidRoom.umap", UE4Version.VER_UE4_25);
             //MessageBox.Show($"Data preserved:{(y.VerifyBinaryEquality() ? "yes" : "no")}");
             //Loop through exports
-            for(int i = 0; i < y.Exports.Count; i++)
+            for (int i = 0; i < y.Exports.Count; i++)
             {
                 Export export = y.Exports[i];
                 if(export is NormalExport ex)
@@ -24,17 +25,20 @@ namespace BlueFireRando
                     //loop through subcategories to find chests/spirits or items
                     for(int j = 0; j < ex.Data.Count; j++)
                     {
-                        if (randomisespirits)
+                        if (ex.Data[j].Name.Equals(FName.FromString("Item")))
+                        {
+                            ex = (NormalExport)z.Exports[207];
+                        }
+                        /*if (randomisespirits)
                         {
                             //BytePropertyData spirit = new BytePropertyData(FName.FromString("Spirit"));
                             if (ex.Data[j].Name.Equals(FName.FromString("Spirit")) && ex.Data[j] is BytePropertyData byt)
                             {
                                 //MessageBox.Show(byt.Value.GetTypeCode().ToString());//returns index in namemap :/
                                 int x=y.AddNameReference(FString.FromString("Spirits::NewEnumerator" + Convert.ToString(13)));
-                                MessageBox.Show(x.ToString());
                                 byt.Value = x;
                             }
-                        } 
+                        } */
                     }
                 }
             }
