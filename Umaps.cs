201ccc97dac625b0e8ba8f8ffc6a5c00070bfc8a
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2021-11-11 10:47:37 +0000

    Spungboimebubbbbbbbbbbbb

--- Umaps.cs
+++ Umaps.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using UAssetAPI;
 using UAssetAPI.PropertyTypes;
 
@@ -10,38 +12,43 @@ namespace BlueFireRando
         {
             //Load umap
             UAsset y = new UAsset(filepath, UE4Version.VER_UE4_25);
-            //MessageBox.Show($"Data preserved:{(y.VerifyBinaryEquality() ? "yes" : "no")}");
+            //MessageBox.Show($"Data preserved: {(y.VerifyBinaryEquality() ? "yes" : "no")} {filepath}");
             //Loop through exports
+            List<string> shit = new List<string>();
             for (int i = 0; i < y.Exports.Count; i++)
             {
                 Export export = y.Exports[i];
                 if (export is NormalExport ex)
                 {
                     //loop through subcategories to find chests/spirits or items
-                    for (int k = 0; k < ex.Data.Count; k++)
+                    for (int j = 0; j < ex.Data.Count; j++)
                     {
-                        for (int j = 0; j < ex.Data.Count; j++)
+                        Random rndm = new Random();
+                        if (ex.Data[j].Name.Equals(FName.FromString("Item")) && ex.Data[j] is BytePropertyData byt)
                         {
-                            Random rndm = new Random();
-                            if (ex.Data[j].Name.Equals(FName.FromString("Item")) && ex.Data[j] is BytePropertyData byt)
-                            {
-                                byt.EnumType = y.AddNameReference(FString.FromString("Items"));
-                                byt.Value = y.AddNameReference(FString.FromString("Items::NewEnumerator" + 14));
-                            }
-                            if (ex.Data[j].Name.Equals(FName.FromString("Spirit")) && ex.Data[j] is BytePropertyData bit)
-                            {
-                                bit.EnumType = y.AddNameReference(FString.FromString("Spirits"));
-                                bit.Value = y.AddNameReference(FString.FromString("Spirits::NewEnumerator" + 13));
-                            }
-                            if (ex.Data[j].Name.Equals(FName.FromString("Ability")) && ex.Data[j] is BytePropertyData bite)
-                            {
-                                bite.EnumType = y.AddNameReference(FString.FromString("Abilities"));
-                                bite.Value = y.AddNameReference(FString.FromString("Abilities::NewEnumerator" + 13));
-                            }
+                            shit.Add(y.GetNameReferenceWithoutZero(byt.Value).ToString());
+                            //byt.EnumType = y.AddNameReference(FString.FromString("Items"));
+                            //byt.Value = y.AddNameReference(FString.FromString("Items::NewEnumerator" + 14));
+                        }
+                        if (ex.Data[j].Name.Equals(FName.FromString("Spirit")) && ex.Data[j] is BytePropertyData bit)
+                        {
+                            bit.EnumType = y.AddNameReference(FString.FromString("Spirits"));
+                            bit.Value = y.AddNameReference(FString.FromString("Spirits::NewEnumerator" + 13));
+                        }
+                        if (ex.Data[j].Name.Equals(FName.FromString("Ability")) && ex.Data[j] is BytePropertyData bite)
+                        {
+                            bite.EnumType = y.AddNameReference(FString.FromString("Abilities"));
+                            bite.Value = y.AddNameReference(FString.FromString("Abilities::NewEnumerator" + 13));
                         }
                     }
                 }
             }
+            string fuck="";
+            foreach (var piceofshit in shit)
+            {
+                fuck += "," + piceofshit;
+            }
+            MessageBox.Show(fuck);
             y.Write(endpath);
         }
     }
