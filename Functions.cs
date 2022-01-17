4f0c239d29bccd5cd47959b38b5fa5a197e102b1
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2022-01-17 19:09:36 +0000

    whoops forgot to commit this

---
+++ Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UAssetAPI;
+
+namespace BlueFireRando
+{
+    public static class Functions
+    {
+        public static void RandomiseEnums(string filepath)
+        {
+            UAsset
+        }
+    }
+}
