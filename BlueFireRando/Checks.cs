879defad2878ff2779579ef03d5ddfd9c734cbee
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2022-01-19 12:10:36 +0000

    Started logging
    Not sure the best way to go about this...

---
+++ BlueFireRando/Checks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BlueFireRando
+{
+    internal class Checks
+    {
+        Dictionary<string, string> checks = new Dictionary<string, string>()
+        {
+            {"Levitate Emote Statue","E_Emotes::NewEnumerator"}
+        };
+    }
+}
