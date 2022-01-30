2712ee6e8b686fadb49023aa302a946f181bea76
bananaturtlesandwich <afcarreyette@gmail.com>
2022-01-30 20:33:31 +0000

    finished up maths

--- BlueFireRando/Asset Editing/Maps.cs
+++ BlueFireRando/Asset Editing/Maps.cs
@@ -23,13 +23,14 @@ public static class Maps
         Directory.CreateDirectory(@".\Randomiser_P\Blue Fire\Content\BlueFire\Maps\World\Voids\17_VoidSanti");
     }
 
-    public static void Shuffle(List<FVector> list,out List<FVector> output)
+    public static void Shuffle(List<FVector> list, out List<FVector> output)
     {
-        Random rndm=new Random();
-        output=new List<FVector>();
-        for(int i = 0; i < list.Count; i++)
+        Random rndm = new Random();
+        output = new List<FVector>();
+        for (int i = 0; i < list.Count; i++)
         {
-            var temp = list[rndm.Next(list.Count)];
+            FVector temp = list[rndm.Next(list.Count)];
+            if (!Vanilla) while (temp.Equals(list[i])) temp = list[rndm.Next(list.Count)];
             output.Add(temp);
             list.Remove(temp);
         }
@@ -44,7 +45,7 @@ public static class Maps
         {
             UAsset map = new UAsset(file, UE4Version.VER_UE4_25);
             //MessageBox.Show($"Data preserved:{(map.VerifyBinaryEquality() ? "yes" : "no")}");
-            foreach (var export in map.Exports)
+            foreach (Export export in map.Exports)
             {
                 if (Emotes) Locations.Add(GetLocation(map, export, "_EmoteStatue_"));
                 if (Spirits) Locations.Add(GetLocation(map, export, "Spirit_"));
@@ -52,22 +53,42 @@ public static class Maps
                 if (Weapons) Locations.Add(GetLocation(map, export, "Chest_"));
             }
         }
-        foreach (var location in Locations) if (location.Equals(new FVector())) Locations.Remove(location);//removes the new Fvectors
+        foreach (FVector location in Locations) if (location.Equals(new FVector())) Locations.Remove(location);//removes the new Fvectors
         return Locations;
     }
 
     public static FVector GetLocation(UAsset map, Export export, string identifier)
     {
-        if (export.ToString().Contains(identifier) && export is NormalExport ex) foreach (var data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (var item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
-        {
-            string[] vector = vec.ToString().Replace('(', ',').Replace(')', ',').Split(',');
-            return new FVector(float.Parse(vector[1]), float.Parse(vector[3]), float.Parse(vector[5]));
-        }
+        if (export.ToString().Contains(identifier) && export is NormalExport ex) foreach (PropertyData data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (PropertyData item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
+                                {
+                                    string[] vector = vec.ToString().Replace('(', ',').Replace(')', ',').Split(',');
+                                    return new FVector(float.Parse(vector[1]), float.Parse(vector[3]), float.Parse(vector[5]));
+                                }
         return new FVector();
     }
 
-    public static void SetAllLocations()
+    public static void SetAllLocations(List<FVector> Locations)
     {
-
+        string[] fileentries = Directory.GetFiles(@".\Baseassets\World", "*.umap", SearchOption.AllDirectories);//Get all the map files
+        foreach (string file in fileentries)
+        {
+            UAsset map = new UAsset(file, UE4Version.VER_UE4_25);
+            foreach(Export export in map.Exports)
+            {
+                if (Emotes) SetLocation(map,export, "_EmoteStatue_", Locations);
+                if (Spirits) SetLocation(map, export, "Spirit_", Locations);
+                if (Tunics) SetLocation(map, export, "_Tunic_", Locations);
+                if (Weapons) SetLocation(map, export, "Chest_", Locations);
+            }
+            map.Write($@"./Randomiser_P/Blue Fire/Content{file.Replace("Baseassets", "")}");
+        }
+    }
+    public static void SetLocation(UAsset map, Export export,string identifier,List<FVector> Locations)
+    {
+        if (export.ToString().Contains(identifier) && export is NormalExport ex) foreach (PropertyData data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (PropertyData item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
+                                {
+                                    vec.Value=Locations[Locations.Count-1];
+                                    Locations.RemoveAt(Locations.Count-1);
+                                }
     }
 }
