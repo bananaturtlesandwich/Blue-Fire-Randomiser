0e4dd64bae878c1cd83b54fbaa8c9ff98076b039
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2022-01-31 08:36:17 +0000

    Object name

--- BlueFireRando/Asset Editing/Maps.cs
+++ BlueFireRando/Asset Editing/Maps.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 //using System.Windows.Forms;
 using UAssetAPI;
 using UAssetAPI.PropertyTypes;
@@ -10,6 +11,9 @@ using static ButtonBools;
 public static class Maps
 {
     public enum Check { Ability, _EmoteStatue_, Spirit_, _Loot_, Tunic, Chest_, Dance_Platform_, Duck, Ember_Angel, NPC }
+
+    public static readonly string[] Ability = { "Chest_A01_Keep_Shield(0)" };
+    public static readonly string[] Spirit = {""};
 
     public static void CreateMapDirectories()
     {
@@ -59,7 +63,7 @@ public static class Maps
 
     public static FVector GetLocation(UAsset map, Export export, string identifier)
     {
-        if (export.ToString().Contains(identifier) && export is NormalExport ex) foreach (PropertyData data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (PropertyData item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
+        if (export.ObjectName.ToString().Contains(identifier) && export is NormalExport ex) foreach (PropertyData data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (PropertyData item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
                                 {
                                     string[] vector = vec.ToString().Replace('(', ',').Replace(')', ',').Split(',');
                                     return new FVector(float.Parse(vector[1]), float.Parse(vector[3]), float.Parse(vector[5]));
@@ -85,10 +89,19 @@ public static class Maps
     }
     public static void SetLocation(UAsset map, Export export,string identifier,List<FVector> Locations)
     {
-        if (export.ToString().Contains(identifier) && export is NormalExport ex) foreach (PropertyData data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (PropertyData item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
+        if (export.ObjectName.ToString().Contains(identifier) && export is NormalExport ex) foreach (PropertyData data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (PropertyData item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
                                 {
                                     vec.Value=Locations[Locations.Count-1];
                                     Locations.RemoveAt(Locations.Count-1);
+                                }
+    }
+
+    public static void SetLocation(UAsset map, Export export, string[] identifier, List<FVector> Locations)
+    {
+        if (identifier.Contains(export.ObjectName.ToString()) && export is NormalExport ex) foreach (PropertyData data in ex.Data) if (data.Name.Equals(FName.FromString("RootComponent")) && data is ObjectPropertyData ob) if (map.Exports[int.Parse(ob.Value.ToString())] is NormalExport norm) foreach (PropertyData item in norm.Data) if (item.Name.Equals(FName.FromString("RelativeLocation")) && item is StructPropertyData struc) if (struc.Value[0].Name.Equals(FName.FromString("RelativeLocation")) && struc.Value[0] is VectorPropertyData vec)
+                                {
+                                    vec.Value = Locations[Locations.Count - 1];
+                                    Locations.RemoveAt(Locations.Count - 1);
                                 }
     }
 }
