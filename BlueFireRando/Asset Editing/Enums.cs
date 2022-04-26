e3f55b7afe45815d3350f17351254a6c96c8f2ae
bananaturtlesandwich <afcarreyette@gmail.com>
2022-04-26 21:54:33 +0100

    Make it less annoying to clone

--- BlueFireRando/Asset Editing/Enums.cs
+++ BlueFireRando/Asset Editing/Enums.cs
@@ -7,10 +7,11 @@ public static class Enums
         UAsset enumerator = new UAsset(filepath, UE4Version.VER_UE4_25);
         if (enumerator.Exports[0] is EnumExport ex)
         {
-            var names = ex.Enum.Names; List<int> used = BannedIndexes.ToList(); int temp;
+            var names = ex.Enum.Names; List<int> used = BannedIndexes.ToList(); 
+            int temp;
             for (int i = 0; i < names.Count; i++) if (!BannedIndexes.Contains(i))
                 {
-                    temp = HelperFunctions.RandInt(names.Count, used);
+                    temp = Helpers.RandInt(names.Count, used);
                     names[i] = new Tuple<FName, long>(names[i].Item1, temp);
                     used.Add(temp);
                 }
