fd7b9c23fdd659af5247a1b99b54a5b4544f1042
spuds <afcarreyette@gmail.com>
2022-05-31 22:00:06 +0100

    motivation back maybe?

--- BlueFireRando/Inventory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlueFireRando;
-public static class Inventory
-{
-    public static bool HasDash;
-    public static bool HasWalljump;
-    public static bool HasHeight;
-    public static byte Keys;
-    public static byte Holykeys;
-}
